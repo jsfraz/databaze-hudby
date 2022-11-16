3e602e2dc93a0ae4345eef34e30d429292391d9e
jsfrz <68445030+jsfraz@users.noreply.github.com>
2022-11-16 19:12:04 +0000

    Znovupřidání možnosti MySQL, bugfixy.

--- songs/songs_text.php
+++ songs/songs_text.php
@@ -3,7 +3,8 @@
 require_once("../tools.php");
 
 $result = querySql("SELECT id_song, name_song, name_album, name_interpret FROM songs INNER JOIN albums ON songs.id_album = albums.id_album INNER JOIN interprets ON songs.id_interpret = interprets.id_interpret;");
-if (hasRows($result)) {
+
+if (countRows($result) > 0) {
     echo '
 <table style="width:100%">
 	<tr>
--- tools.php
+++ tools.php
@@ -1,15 +1,37 @@
 <?php
+//mysql připojení
+function getMysqlConnection() {
+  return mysqli_connect(
+    getenv("servername"),
+    getenv("username"),
+    getenv("password"),
+    getenv("dbname"));
+}
+
+//sqlite "připojení"
+function getSqliteConnection() {
+  return new SQLite3(__DIR__ . "/" . getenv("sqlite_file"));
+}
+
+//vykoná dotaz
 function querySql($sql) {
-  //$conn = getMysql();
-  $db = new SQLite3(__DIR__ . "/database.db");
+  $useMysql = false;    //mysql nebo sqlite
+  $db = null;
+  if ($useMysql) {
+    $db = getMysqlConnection();
+  } else {
+    $db = getSqliteConnection();
+  }
   return $db->query($sql);
 }
 
-function hasRows($result) {
-  $rows = 0;
-  while ($result->fetchArray() && $rows == 0) {
-    $rows++;
-  }
-  return $rows > 0;
+//počet řádků https://stackoverflow.com/questions/48935729/how-to-count-records-in-query-result-on-sqlite3-using-php
+function countRows($result) {
+  $nrows = 0;
+  $result->reset();
+  while ($result->fetchArray())
+    $nrows++;
+  $result->reset();
+  return $nrows;
 }
 ?>
